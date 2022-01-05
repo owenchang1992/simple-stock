325f9826ecb7b1ea3dabbca9b08821e838fbfcf9
Owen Chang <owenchang.dev@gmail.com>
2022-01-05 21:40:21 +0800

    feature: update test file

--- components/StockTitle/StockTitle.tsx
+++ components/StockTitle/StockTitle.tsx
@@ -4,10 +4,9 @@ interface Props {
   metaData: {
     ['1. Information']?: string,
     ['2. Symbol']: string,
-    ['3. Last Refreshed']: string,
-    ['4. Interval']: string,
-    ['5. Output Size']: string,
-    ['6. Time Zone']: string,
+    ['3. Last Refreshed']?: string,
+    ['4. Output Size']?: string,
+    ['5. Time Zone']?: string,
   }
 }
 
